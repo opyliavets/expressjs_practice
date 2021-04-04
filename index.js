a0ad4873993b7735f957f49f2ebdb1fa332d5cd5
Oleksandr Pyliavets <o,pyliavets@gmail.com>
2021-04-04 19:50:35 +0300

    ST4. Added basket and relative functional

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
+const cardRoutes = require('./routes/card')
 const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
@@ -16,12 +17,13 @@ app.engine('hbs', hbs.engine) // реєстрація движка hbs
 app.set('view engine', 'hbs') // конфігурація для його використання
 app.set('views', 'views',) 
 
-app.use(express.static('public')) //реєстрація папки  public як статичної, для доступу до неї express js
+app.use(express.static(path.join(__dirname, 'public'))) //реєстрація папки  public як статичної, для доступу до неї express js
 app.use(express.urlencoded({extended:true}))
 
 app.use('/',homeRoutes) //використання роутингу для відповідної сторінки
 app.use('/add',addRoutes) //використання роутингу для відповідної сторінки
 app.use('/courses', coursesRoutes) //використання роутингу для відповідної сторінки
+app.use('/card', cardRoutes)
 
 const PORT = process.env.PORT || 3000
 
